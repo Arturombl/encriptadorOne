e9a4a3f059383cceb75a706db744723f2b566f31
arturombl <arturomobl@gmail.com>
2024-02-28 22:40:50 -0600

    Creamos una funcion para las alertas y mejoramos su visual agregando la libreria SweetAlert2

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const textarea = document.querySelector('textarea');
 
 //funcion para validar si solo se escribieron letras minusculas sin acento
 function validar(){
-    const regExp = /[a-z]$/;
+    const regExp = /[A-Z]/;
     const input = document.forms['formulario']['mensajeUsuario'];
+    console.log(!input.value)
+    console.log(!regExp.test(input.value))
     if(!input.value) {
         return false;
       } else {
-        if(!regExp.test(input.value)){
+        if(regExp.test(input.value)){
         return false
         }else
         return true;
@@ -26,16 +28,18 @@ function validar(){
 
 //funcion para encriptar
 function encriptar() {
-   encriptarDesencriptar(letras, silavas);
+   encriptarDesencriptar(letras, silavas, 'ent');
 }
 
 //funcion para desencriptar
 function desencriptar() {
-    encriptarDesencriptar(silavas, letras)
+    encriptarDesencriptar(silavas, letras, 'des')
 }
 
 //funcion proceso de encriptado y desencriptado
-function encriptarDesencriptar(arreglo1, arreglo2){
+function encriptarDesencriptar(arreglo1, arreglo2, tipo){
+    console.log(tipo);
+    alerta(tipo);
     let isValid = validar();
     mensaje = document.getElementById('mensajeUsuario').value;
     if(isValid === true){
@@ -43,10 +47,17 @@ function encriptarDesencriptar(arreglo1, arreglo2){
             mensaje = mensaje.replaceAll(arreglo1[i],arreglo2[i]);
             console.log(mensaje);
         }
+
         restablecer();
         return mostrarMensaje(mensaje)
     }else{
-        alert('Solo se admiten letras minusculas y sin acentos, Igresa otro mensaje');
+       // alert('Solo se admiten letras minusculas y sin acentos, Igresa otro mensaje');
+       Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "solo letras minusculas, intente de nuevo!",
+      });
+      restablecer();
     }
     restablecer();
 }
@@ -76,6 +87,28 @@ function copiarTexto(){
     content.select();
     content.setSelectionRange(0,99999);
     navigator.clipboard.writeText(content.value);
-
-    alert('El texto se a copiado ');
+    alerta('cop');
+}
+function alerta(tipo){
+    console.log(tipo);
+    let mensajeAlerta;
+    switch (tipo){
+        case 'enc':
+            mensajeAlerta = 'Encriptado Exitoso';
+            break;
+        case 'des':
+            mensajeAlerta = 'Desencriptado Exitoso';
+            break;
+        case 'cop':
+            mensajeAlerta = 'Copiado Exitoso';
+            break;
+    }
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: mensajeAlerta,
+        showConfirmButton: false,
+        timer: 1500
+      });
+      return;
 }
